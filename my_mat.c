05caf0d80b00bed30ea55effeb7bac1b00b622cb
Naama Shiponi <naamas1234567@gmail.com>
2022-12-06 21:01:45 +0200

    cheng if in my_mat

--- my_mat.c
+++ my_mat.c
@@ -16,21 +16,19 @@ int findingTheShortestDistance(int distanceMatrix[][MATRIX_SIZE])
                     // If j==k according to algorithms there is nothing to check
                     /* If the matrix at position (i,k) or (k,j) is equal to 0 by definition,
                        this means that there is no way to one of the nodes and therefore there is nothing to continue to check */
-                    if ((j == k) || (i == j) || (distanceMatrix[i][k] == 0) || (distanceMatrix[k][j] == 0))
+                    if ((j != k) && (i != j) && (distanceMatrix[i][k] != 0) && (distanceMatrix[k][j] != 0))
                     {
-                        continue;
-                    }
+                        // If the matrix at position (i,j) is equal to 0 this means that there is no way between the nodes
+                        if ((distanceMatrix[i][j] == 0))
+                        {
+                            distanceMatrix[i][j] = distanceMatrix[i][k] + distanceMatrix[k][j];
+                        }
 
-                    // If the matrix at position (i,j) is equal to 0 this means that there is no way between the nodes
-                    if ((distanceMatrix[i][j] == 0))
-                    {
-                        distanceMatrix[i][j] = distanceMatrix[i][k] + distanceMatrix[k][j];
-                    }
-
-                    // The weight (distance) between node i and node j is lighter (shorter) when passing through node k
-                    if ((distanceMatrix[i][k] + distanceMatrix[k][j]) < distanceMatrix[i][j])
-                    {
-                        distanceMatrix[i][j] = distanceMatrix[i][k] + distanceMatrix[k][j];
+                        // The weight (distance) between node i and node j is lighter (shorter) when passing through node k
+                        if ((distanceMatrix[i][k] + distanceMatrix[k][j]) < distanceMatrix[i][j])
+                        {
+                            distanceMatrix[i][j] = distanceMatrix[i][k] + distanceMatrix[k][j];
+                        }
                     }
                 }
             }
